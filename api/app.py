c54fb7f307dc7105a88e82c8c36c06043451234d
Mohit Saxena <132200642+m22aie240@users.noreply.github.com>
2023-12-11 11:15:46 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,41 +2,34 @@ from flask import Flask, request, jsonify
 from joblib import load
 
 app = Flask(__name__)
-model_path = "models/svm_gamma:0.01_C:1.joblib"
-model = load(model_path)
+
+# Loading multiple models
+models = {
+    'svm': load("models/svm_gamma:0.01_C:1.joblib"),
+    'lr': load("models/lr_solver:newton-cg.joblib"),  
+    'tree': load("models/tree_max_depth:50.joblib")  
+}
 
 @app.route("/")
 def hello_world():
     return "<b>Hello, World!</b>"
 
-@app.route("/sum", methods=['POST'])
-def sum_numbers():
-    data = request.json
-    x = data.get('x')
-    y = data.get('y')
-    if x is None or y is None:
-        return jsonify({"error": "Missing x or y parameter"}), 400
-    z = x + y
-    return jsonify({'sum': z})
+#  
 
-@app.route("/predict", methods=['POST'])
-def predict():
+@app.route("/predict/<model_type>", methods=['POST'])
+def predict(model_type):
+    if model_type not in models:
+        return jsonify({"error": "Model type not supported"}), 400
+
     image = request.json.get('image')
     if image is None:
         return jsonify({"error": "Missing image data"}), 400
+
+    model = models[model_type]
     predicted = model.predict([image])
     return jsonify({"y_predicted": int(predicted[0])})
 
-@app.route("/compare", methods=['POST'])
-def compare():
-    data = request.json
-    image1 = data.get('image1')
-    image2 = data.get('image2')
-    if image1 is None or image2 is None:
-        return jsonify({"error": "Missing image1 or image2 data"}), 400
-    predicted1 = model.predict([image1])
-    predicted2 = model.predict([image2])
-    return jsonify({"are_same_digit": int(predicted1[0]) == int(predicted2[0])})
+# ... [rest of your Flask app]
 
 if __name__ == '__main__':
     app.run(debug=True)
